520f2280e1ce15f31cdec023c667c1c0a230cf55
Tim Chumbash <posseslash@gmail.com>
2020-06-14 23:04:09 +0200

    added proptypes and card rendering

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from "./components/app/app.jsx";
 
 const settings = {
   places: 777,
+  descriptions: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `This place is amazing`, `Terrible locations`, `Landlord is a douche`],
 };
 
 ReactDOM.render(
     <App
       places = {settings.places}
+      descriptions = {settings.descriptions}
     />,
     document.querySelector(`#root`)
 );
